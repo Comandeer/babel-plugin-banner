30e22b7b8e5ee47a969bcdbe053ae5ed9c985fdb
Tomasz Jakut <vepomoc@gmail.com>
2018-02-11 15:07:12 +0100

    chore(dist): clean after release [ci skip]

--- dist/babel-plugin-banner.js
+++
@@ -1,3 +0,0 @@
-/*! @comandeer/babel-plugin-banner v2.0.0 | (c) 2018 Comandeer | MIT license (see LICENSE) */
-'use strict';var oneLineRegex=/^\/\/(.*?)\n?$/,multiLineRegex=/^\/\*([\s\S]*?)\*\/$/;function isComment(a){return!!(oneLineRegex.test(a)||multiLineRegex.test(a))}function getCommentContent(a){return oneLineRegex.test(a)?a.replace(/^\/\//,''):a.replace(/^\/\*/,'').replace(/\*\/$/,'')}function babelPlugin(a){var b=a.types,c=b.noop;return{visitor:{Program:function(a,b){var d=b.opts,e=d.banner,f=d.newLine;if('string'!=typeof e||!isComment(e))throw new TypeError('Banner must be a valid comment.');(!(f!==void 0)||f)&&a.unshiftContainer('body',c()),a.addComment('leading',getCommentContent(e))}}}}module.exports=babelPlugin;
-//# sourceMappingURL=babel-plugin-banner.js.map
