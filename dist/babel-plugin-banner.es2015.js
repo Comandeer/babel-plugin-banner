ebe4c3b436c8fbfa0b537706a2a397405bab9bcf
Comandeer <vepomoc@gmail.com>
2019-06-29 20:59:02 +0200

    5.0.0

---
+++ dist/babel-plugin-banner.es2015.js
@@ -0,0 +1,3 @@
+/*! @comandeer/babel-plugin-banner v5.0.0 | (c) 2019 Comandeer | MIT license (see LICENSE) */
+const oneLineRegex=/^\/\/(.*?)\n?$/,multiLineRegex=/^\/\*([\s\S]*?)\*\/\n?$/;function isComment(a){return!!(oneLineRegex.test(a)||multiLineRegex.test(a))}function getCommentContent(a){return oneLineRegex.test(a)?a.replace(/^\/\//,""):a.replace(/^\/\*/,"").replace(/\*\/\n?$/,"")}function babelPlugin(){return{visitor:{Program:function(a,{opts:b}){const c=b.banner;if("string"!=typeof c||!isComment(c))throw new TypeError("Banner must be a valid comment.");a.addComment("leading",getCommentContent(c))}}}}export default babelPlugin;export{isComment,getCommentContent};
+//# sourceMappingURL=babel-plugin-banner.es2015.js.map
